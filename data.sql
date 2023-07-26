b0bc84f19adeb812b77e4eb58b45b26c3ebdc1a7
sandeepsinha1002 <110661796+sandeepsinha1002@users.noreply.github.com>
2023-07-26 15:14:14 +0530

    Update data.sql

--- data.sql
+++ data.sql
@@ -1,56 +1,40 @@
 CREATE TABLE USER(
-    firstName VARCHAR(50),
-    lastName VARCHAR(50),
+    user_id int PRIMARY KEY,
+    firstName VARCHAR(50) NOT NULL,
+    lastName VARCHAR(50) NOT NULL,
     email VARCHAR(50) PRIMARY KEY,
-    passwword PASSWORD(12),
+    password PASSWORD(12) NOT NULL,
+    streetAddress VARCHAR(50) NOT NULL,
+    city VARCHAR(20) NOT NULL,
+    state VARCHAR(20) NOT NULL,
+    pincode int(6) NOT NULL,
+    phoneNumber int(10) UNIQUE,
 );
 
-CREATE TABLE ADDRESS(
-    address_id INT PRIMARY KEY,
-    streetAddress VARCHAR(50),
-    city VARCHAR(20),
-    state VARCHAR(20),
-    pincode int(6),
-    phoneNumber int(10),
-);
-
-CREATE TABLE SIZE(
-    size_id int PRIMARY KEY,
-    name varchar(50),
-
-);
 CREATE TABLE CATEGORY(
   category_id INT PRIMARY KEY,
-  category_name VARCHAR(50),
+  category_name VARCHAR(50) NOT NULL,
   parent_id INT,
   level INT,
   FOREIGN KEY (parent_id) REFERENCES category (category_id)
 
 );
-CREATE TABLE RATING (
-    rating_id INT PRIMARY KEY,
-    rating int,
-);
 CREATE TABLE REVIEW(
     review_id INT PRIMARY KEY,
-    text varchar(100),
-    rating_id int
-    FOREIGN KEY(rating_id) REFERENCES RATING(rating_id),
+    text varchar(100) ,
+    rating int CHECK(rating>=0 AND rating<=5) ;
 );
 
 CREATE TABLE PRODUCT(
-    ID int(5) PRIMARY KEY,
-    title varchar(50),
-    description varchar(200),
-    price int(10),
-    discountedPrice int(10),
-    discountedPercentage int(3),
-    quantity int(6),
-    brand varchar(20),
-    color varchar(10),
-    size_id int,
-    FOREIGN KEY(size_id) REFERENCES SIZE(size_id)
-    imageUrl varchar(50),
+    product_id int(5) PRIMARY KEY,
+    title varchar(50) NOT NULL,
+    description varchar(200) NOT NULL,
+    price int(10) NOT NULL,
+    quantity int(6) NOT NULL,
+    brand varchar(20) NOT NULL,
+    color varchar(10) NOT NULL,
+    size varchar(20) NOT NULL;
+    imageUrl varchar(50) NOT NULL,
     review_id int
     FOREIGN KEY(review_id) REFERENCES REVIEW(review_id)
     category_id INT,
@@ -58,8 +42,51 @@ CREATE TABLE PRODUCT(
 );
 
 CREATE TABLE PAYMENTINFO(
-    email varchar(12),
+    email varchar(50),
     FOREIGN KEY(email) REFERENCES USER(email),
     address_id INT,
     FOREIGN KEY(address_id) REFERENCES ADDRESS(address_id),
 );
+
+CREATE TABLE SALES_REPORT (
+    sales_id INT PRIMARY KEY,
+    report_date DATE ,
+    total_sales DECIMAL(10, 2) NOT NULL,
+);
+
+CREATE TABLE MONTHLY_REVENUE (
+    revenue_id INT PRIMARY KEY,
+    revenue_month DATE ,
+    total_revenue DECIMAL(10, 2) NOT NULL,
+);
+
+
+CREATE TABLE CART{
+    cart_id int PRIMARY KEY
+    email varchar(50),
+    FOREIGN KEY(email) REFERENCES USER(email),
+    total_price int,
+    total_items int CHECK(total_items<=10),
+}
+
+CREATE TABLE TOP_SELLING_PRODUCTS (
+    top_product_id INT PRIMARY KEY,
+    product_name VARCHAR(100),
+    total_sold INT
+);
+
+CREATE TABLE CUSTOMER_REPORT (
+    user_id int,
+    FOREIGN KEY(user_id) REFERENCES USER(user_id),
+    report_id INT PRIMARY KEY,
+    registration_month DATE ,
+    new_customers INT 
+);
+
+CREATE TABLE ORDER_REPORT (
+    order_id INT PRIMARY KEY,
+    order_month DATE,
+    total_orders INT,
+    average_order_value DECIMAL(10, 2) 
+);
+
