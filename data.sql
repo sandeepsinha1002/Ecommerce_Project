3a76c368c34c24e6c8a2b6bebf1e0b9dd128f746
sandeepsinha1002 <110661796+sandeepsinha1002@users.noreply.github.com>
2023-07-28 10:55:47 +0530

    Update data.sql

--- data.sql
+++ data.sql
@@ -22,6 +22,7 @@ CREATE TABLE USER_ADDRESS(
 CREATE TABLE PRODUCT(
     product_id int,
     category_id int,
+    FOREIGN KEY(category_id) REFERENCES CATEGORY(category_id),
     product_name varchar(50),
     description varchar(100),
     imageUrl varchar(50),
@@ -40,24 +41,19 @@ CREATE TABLE CATEGORY(
     category_id INT,
     category_name varchar(50) NOT NULL,
 )
-CREATE TABLE VARIATION(
-    variation_id int PRIMARY KEY,
+CREATE TABLE ATTRIBUTES(
+    attributes_id int PRIMARY KEY,
     category_id int,
     FOREIGN KEY(category_id) REFERENCES CATEGORY(category_id),
-    name varchar(50) NOT NULL,
+    colour varchar(50) NOT NULL,
+    size varchar(20) NOT NULL,
 );
-CREATE TABLE VARIATION_OPTION(
-    variation_option_id int PRIMARY KEY,
-    variation_id int,
-    FOREIGN KEY(variation_id) REFERENCES VARIATION(variation_id),
-    value int NOT NULL,
- );
 
 CREATE TABLE PRODUCT_CONFIGURATION(
     product_item_id int,
     FOREIGN KEY(product_item_id) REFERENCES PRODUCT_ITEM(product_item_id),
-    variation_option_id int,
-    FOREIGN KEY(variation_option_id) REFERENCES VARIATION_OPTION(variation_option_id),+
+    attributes_id int,
+    FOREIGN KEY(attributes_id) REFERENCES ATTRIBUTES(attributes_id),
 );
 
 CREATE TABLE CART(
@@ -79,19 +75,11 @@ CREATE TABLE PAYMENT_METHOD(
     payment_method_id int PRIMARY KEY,
     user_id int,
     FOREIGN KEY(user_id) REFERENCES USER(user_id),
+    order_total int,
+    FOREIGN KEY(order_total) REFERENCES ORDER_PRODUCT(order_total),
     account_no int NOT NULL,
     expiry_date date NOT NULL,
     
-)
-
-CREATE TABLE SHIPPING_METHOD(
-    shipping_method_id int PRIMARY KEY,
-    method_type varchar(20),
-)
-
-CREATE TABLE ORDER_STATUS(
-    order_status_id int,
-    status varchar(50) NOT NULL,
 )
 CREATE TABLE ORDER_PRODUCT(
     order_product_id int PRIMARY KEY,
@@ -102,11 +90,10 @@ CREATE TABLE ORDER_PRODUCT(
     FOREIGN KEY(payment_method_id) REFERENCES PAYMENT_METHOD(payment_method_id),
     address_id int,
     FOREIGN KEY(address_id) REFERENCES ADDRESS(address_id),
-    shipping_method_id int,
-    FOREIGN KEY(shipping_method_id) REFERENCES SHIPPING_METHOD(shipping_method_id),
-    order_total int NOT NULL,
-    order_status_id int,
-    FOREIGN KEY(order_status_id) REFERENCES ORDER_STATUS(order_status_id),
+    order_total int NOT NULL PRIMARY KEY,
+    shipping_method_type varchar(20),
+    order_status varchar(50) NOT NULL,
+
 );
 
 CREATE TABLE REVIEW(
@@ -118,5 +105,13 @@ CREATE TABLE REVIEW(
     rating int CHECK(rating>=0 AND rating<=5) NOT NULL,
     comment varchar(100),
 )
-                    
+
+ 
+
+
+
+
+
+
+
 
